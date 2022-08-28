2d1fe8515a05eb7a52fe6e5ff09b43175604455b
sinapot <omai_edison@yahoo.com>
2022-08-28 12:53:41 +1200

    added scenario to Intercept and mock the REQUEST to server

--- cypress/e2e/BDD-OrangeHRM/cYintercept.feature
+++ cypress/e2e/BDD-OrangeHRM/cYintercept.feature
@@ -1,6 +1,9 @@
 Feature: spying and stubbing network calls using cy.intercept
   
-  Scenario: Intercept request and stub one person
-    Given I'm logged in
+  Scenario: Intercept and mock the RESPONSE
     When Search for developers
     Then I should see only one developer
+
+  Scenario: Intercept and mock the REQUEST
+    When Modify the request to server as a Job Title ID
+    Then server response should be for the new Job Title
--- cypress/e2e/BDD-OrangeHRM/cYintercept.js
+++ cypress/e2e/BDD-OrangeHRM/cYintercept.js
@@ -1,10 +1,13 @@
-const { When, Then, Given } = require("@badeball/cypress-cucumber-preprocessor");
+const { When, Then, Given, And, Before } = require("@badeball/cypress-cucumber-preprocessor");
 import { loginPage } from "./pageobjects/loginPage";
 import { pimPage } from "./pageobjects/employeeList";
 
-Given("I'm logged in", () => {
-    loginPage.loginsession();
+Before(()=>{
+   //use page object to login, cy.session is also captured there
+   loginPage.loginsession();
 })
+
+
 When("Search for developers", () => {
     //setup to intercept the network call
     cy.intercept('GET','**/pim/employees*',
@@ -50,4 +53,23 @@ When("Search for developers", () => {
 Then("I should see only one developer", () => {
     cy.get(".orangehrm-paper-container").contains("Software Engineer").should("be.visible");
 
+})
+
+When ("Modify the request to server as a Job Title ID",()=>{
+   //Controlling the outgoing request
+   cy.intercept('GET','**/pim/employees*',
+      (req) =>{
+         //changing request call from jobTitleId=7 (Software Engineer) to jobTitleId=9 (QA Engineer)
+         req.url = 'https://opensource-demo.orangehrmlive.com/web/index.php/api/v2/pim/employees?limit=50&offset=0&model=detailed&includeEmployees=onlyCurrent&jobTitleId=9&sortField=employee.firstName&sortOrder=ASC'
+         req.continue()
+      }
+   ).as("newJobTitle9")
+   //search for Software Engineer
+   pimPage.clickJobTitleDropown();
+   //intercept and change the server request to QA Engineer
+   cy.wait("@newJobTitle9")
+})
+Then ("server response should be for the new Job Title", ()=>{
+   //verify the server responded with QA Engineers
+   cy.get(".orangehrm-paper-container").contains("QA Engineer").should("be.visible");
 })
